132744fbc9a19123cc5155be04a56f9f7d6c7f05
LABORATÓRIO DE CIÊNCIAS / IHAC <labihac@ufba.br>
2019-04-17 09:12:18 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var game=new Phaser.Game(800, 600, Phaser.CANVAS, 'game', {preload: preload, create: create, update: update});
+
+function preload(){
+ 
+}
+function create(){
+  
+}
+function update(){
+
+}
